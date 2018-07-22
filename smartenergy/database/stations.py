f9fab11f5bc83335a3b721bd2da6530ffcd2ea82
javiermas <javier.mas@barcelonagse.eu>
2018-07-22 19:54:01 +0200

    :hammer: :bug: Modify load_single_station method in Stations
    Now it returns a Series instead of a DataFrame. Also fixed bug in argument default.

--- smartenergy/database/stations.py
+++ smartenergy/database/stations.py
@@ -1,15 +1,16 @@
-from pandas import DataFrame
+from pandas import Series
 from .mongo_collection import MongoCollection
 from .connection import Connection
 
 
 class Stations(MongoCollection):
 
-    def __init__(self, connection=Connection()):
+    def __init__(self, connection=Connection().get_connection()):
         super().__init__('stations', connection)
 
     def load_single_station(self, station_id):
-        return DataFrame(list(self.coll.find({'solbox_id': station_id}))).set_index('solbox_id')
+        station_data = Series(list(self.coll.find({'solbox_id': station_id}))[0])
+        return station_data.drop('_id')
 
     @property
     def station_ids(self):
