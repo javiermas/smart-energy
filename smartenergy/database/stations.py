847d7fe31d2306b721333bc7ae05b38e584726e3
javiermas <javier.mas@barcelonagse.eu>
2018-07-25 21:37:35 +0200

    :sparkles: Override load method in Stations

--- smartenergy/database/stations.py
+++ smartenergy/database/stations.py
@@ -1,4 +1,4 @@
-from pandas import Series
+from pandas import Series, DataFrame
 from .mongo_collection import MongoCollection
 from .connection import Connection
 
@@ -7,6 +7,9 @@ class Stations(MongoCollection):
 
     def __init__(self, connection=Connection().get_connection()):
         super().__init__('stations', connection)
+
+    def load(self, nrows=0):
+        return DataFrame(list(self.coll.find().limit(nrows))).drop('_id', axis=1)
 
     def load_single_station(self, station_id):
         station_data = Series(list(self.coll.find({'solbox_id': station_id}))[0])
