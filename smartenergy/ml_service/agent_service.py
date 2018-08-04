224a88c4bd6904db6f48cc8328e35d324007dc7a
javiermas <javier.mas@barcelonagse.eu>
2018-08-04 17:49:41 +0200

    :sparkles: Add call method and agent argument to init

--- smartenergy/ml_service/agent_service.py
+++ smartenergy/ml_service/agent_service.py
@@ -3,11 +3,15 @@ from .base import Service
 
 class AgentService(Service):
 
-    def __init__(self):
-        self.action_space = None
+    def __init__(self, agent):
+        self.agent = agent
+        self.action_space = self.agent.action_space
+
+    def __call__(self, *args, **kwargs):
+        return self.get_action(*args, **kwargs)
 
     def train(self):
         pass
 
-    def get_action(self):
+    def get_action(self, state):
         pass
