09aa20d5b248caef97b615502061828edec73a7e
javiermas <javier.mas@barcelonagse.eu>
2018-08-04 17:55:56 +0200

    :sparkles: Develop run_environment script

--- smartenergy/run_environment.py
+++ smartenergy/run_environment.py
@@ -1,11 +1,11 @@
+from datetime import timedelta
 from smartenergy.network import Network, Pipe, installation
-from smartenergy.ml_service import MLService
-from smartenergy.environments import SBEnvironment
+from smartenergy.ml_service import MLService, FeatureService, ForecastService, AgentService
+from smartenergy.ml_service.features import LaggedReadings
+from smartenergy.ml_service.models import XGBoostHourlyGenerationStationPredictor, BasicAgent
+from smartenergy.environments.sb_environment import SBEnvironment
 from smartenergy.database import HourlyMeasurements, Stations
 
-
-init_t = 0
-step_size = 1
 
 stations = Stations()
 station_ids = stations.station_ids
@@ -28,7 +28,44 @@ for _id in station_ids:
     _installation = installation.Installation(_id, installation_elements, connection_to_grid)
     network.add_installation(_installation)
 
-ml_service = MLService(network.action_space)
+#  ML_service
+hourly_measurements = HourlyMeasurements()
+init_t = hourly_measurements.get_first_measurement()[0]['datetime'] + timedelta(days=10)
+step_size = timedelta(hours=1)
+
+lags = 24
+features = [LaggedReadings(lags=24)]
+feature_service = FeatureService(features)
+models = [XGBoostHourlyGenerationStationPredictor(station_id=station_id) for station_id in station_ids]
+forecast_service = ForecastService(models)
+action_space = {
+    f'installation_{i}': {
+        'generator_covered_energy': range(2),
+        'transaction_with_energy': range(3),
+    } for i in range(len(station_ids))}
+
+agent_parameters = {
+    'epsilon': 0.1,
+}
+network_parameters = {
+    'learning_rate': 0.01,
+}
+
+non_tunable_parameters = {
+    'action_space': action_space,
+    'state_size': 20,
+}
+
+parameters = {**agent_parameters, **network_parameters, **non_tunable_parameters}
+
+basic_agent = BasicAgent(**parameters)
+agent_service = AgentService(basic_agent, action_space)
+
+ml_service = MLService(
+    feature_service=feature_service,
+    forecast_service=forecast_service,
+    agent_service=agent_service,
+)
 
 sb_environment = SBEnvironment(
     ml_service=ml_service,
@@ -38,5 +75,3 @@ sb_environment = SBEnvironment(
 )
 
 sb_environment.run(steps=100)
-
-
