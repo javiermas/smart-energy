5a2dc3a1c41dcad16804e0b15dcbad8168c7b85a
javiermas <javier.mas@barcelonagse.eu>
2018-08-11 18:32:23 +0200

    :hammer: :bug: Refactor and fix bug in BasicAgent
    Replace network args by kwargs and call tf session with networks graph

--- smartenergy/ml_service/models/agents/basic_agent.py
+++ smartenergy/ml_service/models/agents/basic_agent.py
@@ -4,14 +4,14 @@ from .shallow_network import ShallowNetwork
 
 class BasicAgent(object):
 
-    def __init__(self, epsilon, action_space, state_size, learning_rate):
+    def __init__(self, epsilon, action_space, state_size, **network_kwargs):
         self.action_space = action_space
         self.state_size = state_size
-        self.shallow_network = ShallowNetwork(self.action_space, state_size, learning_rate)
+        self.shallow_network = ShallowNetwork(self.action_space, state_size, **network_kwargs)
         self.shallow_network.initialize()
-    
+
     def get_action(self, state):
-        with tf.Session() as sess:
+        with tf.Session(graph=self.shallow_network.graph) as sess:
             action_nodes = [node for nodes in self.shallow_network.actions.values()
                             for node in nodes.values()]
             reward_nodes = [node for nodes in self.shallow_network.reward.values()
@@ -22,8 +22,6 @@ class BasicAgent(object):
             }
             actions, expected_rewards = actions, expected_rewards = sess.run(nodes, feed)
 
-
-
     def get_random_action(self):
         actions = {}
         for name, sub_space in self.action_space.items():
@@ -32,7 +30,3 @@ class BasicAgent(object):
                 actions[name][sub_space_name] = choice(range(sub_space_value))
 
         return actions
-
-
-
-
