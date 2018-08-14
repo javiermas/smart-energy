8cd8ad1322a199f75664b6a1244054c86b22f990
javiermas <javier.mas@barcelonagse.eu>
2018-08-14 13:31:31 +0200

    :bug: :sparkles: Finish first runnable version of the network
    Returns nan actions

--- smartenergy/ml_service/models/agents/shallow_network.py
+++ smartenergy/ml_service/models/agents/shallow_network.py
@@ -4,27 +4,26 @@ from .base import Network
 
 class ShallowNetwork(Network):
 
-    def __init__(self, action_space: dict, state_size: int, hidden_units: int, learning_rate: float):
+    def __init__(self, action_space: dict, hidden_units: int, learning_rate: float):
         self.action_space = action_space
-        self.state_size = state_size
         self.hidden_units = hidden_units
         self.learning_rate = learning_rate
         self.num_actions = sum([len(elem_values) for key, value in action_space.items()
                                 for elem_keys, elem_values in value.items()])
-        self.initializer = tf.contrib.layers.xavier_initializer()
-        #self.initializer = tf.random_normal
+        self.initializer = tf.random_normal
 
     def initialize(self):
         tf.reset_default_graph()
         self.graph = tf.Graph()
         with self.graph.as_default():
-            self.state = tf.placeholder(tf.float32, [1, 1], name='state')
+            self.state = tf.placeholder(tf.float32, [1, None], name='state')
             #reward = tf.placeholder(tf.float32, None, name='reward')
             #next_state = tf.placeholder(tf.float32, [None, self.state_size], name='next_state')
             # Replace Q by reward 
-            state_size = tf.placeholder(tf.int32)
-            W = tf.Variable(self.initializer(shape=[state_size, self.hidden_units * self.num_actions]),
-                            name='common_weights', validate_shape=False)
+            W = tf.Variable(
+                self.initializer(shape=[tf.shape(self.state)[1], self.hidden_units * self.num_actions]),
+                name='common_weights',
+                validate_shape=False)
             b = tf.Variable(self.initializer(shape=[self.hidden_units * self.num_actions]),
                             name='common_bias', validate_shape=False)
             common_layer = tf.nn.relu(tf.matmul(self.state, W) + b, name='common_layer')
@@ -36,8 +35,9 @@ class ShallowNetwork(Network):
                         tf.float32, [1, len(self.action_space[key][sub_space_key])],
                         name=f'Q_next_{key}_{sub_space_key}')
                     weights[key][sub_space_key] = tf.Variable(
-                        self.initializer([int(common_layer.shape[1]), max(sub_space_value)]),
-                        name=f'weights_{key}_{sub_space_key}')
+                        self.initializer(shape=[tf.shape(common_layer)[1], max(sub_space_value)]),
+                        name=f'weights_{key}_{sub_space_key}',
+                        validate_shape=False)
                     _Q_out = tf.matmul(common_layer, weights[key][sub_space_key])
                     actions[key][sub_space_key] = tf.argmax(_Q_out)
                     Q_out[key][sub_space_key] = _Q_out
