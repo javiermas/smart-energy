fa7085d357b6fc4ee7ee23d2fac8493d7c6b7fd6
javiermas <javier.mas@barcelonagse.eu>
2018-08-21 19:12:33 +0200

    :sparkles: :refactor: Add method to turn dict to list of tuples to reconstruct action_space

--- smartenergy/ml_service/models/agents/basic_agent.py
+++ smartenergy/ml_service/models/agents/basic_agent.py
@@ -10,19 +10,21 @@ class BasicAgent(object):
         self.shallow_network = ShallowNetwork(self.action_space, **network_kwargs)
         self.shallow_network.initialize()
 
+    def train(self):
+
     def get_action(self, state):
         with tf.Session(graph=self.shallow_network.graph) as sess:
-            action_nodes = [node for nodes in self.shallow_network.actions.values()
-                            for node in nodes.values()]
-            reward_nodes = [node for nodes in self.shallow_network.reward.values()
-                            for node in nodes.values()]
-            nodes = action_nodes + reward_nodes
+            action_nodes = self.dict_to_list_of_tuples(self.shallow_network.actions)
+            reward_nodes = self.dict_to_list_of_tuples(self.shallow_network.reward)
+            nodes = [node[2] for node in action_nodes + reward_nodes]
             feed = {
                 self.shallow_network.state: state,
             }
             sess.run([tf.global_variables_initializer()], feed)
             output = sess.run(nodes, feed)
-            actions, expected_rewards = self.split_list(output, 2)
+            actions, exp_rewards = self.split_list(output, 2)
+            actions = {node[0]: {node[1]: action} for node, action in zip(action_nodes, actions)}
+            exp_rewards = {node[0]: {node[1]: reward} for node, reward in zip(reward_nodes, exp_rewards)}
             return actions
 
     def get_random_action(self):
@@ -37,3 +39,8 @@ class BasicAgent(object):
     @staticmethod
     def split_list(_list, n_splits=1):
         return [_list[i * len(_list) // n_splits: (i + 1) * len(_list) // n_splits] for i in range(n_splits)]
+    
+    @staticmethod
+    def dict_to_list_of_tuples(_dict):
+        lists = [[tuple([key]) + node for node in list(nodes.items())] for key, nodes in _dict.items()]
+        return [elem for sublist in lists for elem in sublist]
