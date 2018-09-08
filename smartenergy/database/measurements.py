0dc228d613fcab9b527d5d54037f8921d18a2c46
javiermas <javier.mas@barcelonagse.eu>
2018-09-08 20:45:59 +0200

    :hammer: Refactor loading mewthods of Measurements to follow all-multiple-single system

--- smartenergy/database/measurements.py
+++ smartenergy/database/measurements.py
@@ -1,24 +1,29 @@
 from pandas import DataFrame, concat
+
 from .mongo_collection import MongoCollection
 
 
 class Measurements(MongoCollection):
 
     def __init__(self, collection, connection):
-        super(Measurements, self).__init__(collection, connection)
+        super().__init__(collection, connection)
 
     def load(self, nrows=0):
         return DataFrame(list(self.coll.find().limit(nrows))).drop('_id', axis=1)
 
-    def load_single_station(self, station_id, nrows=None):
-        return DataFrame(list(self.coll.find({'solbox_id': station_id}).limit(nrows)))
+    def load_single_station(self, station_id, limit=0):
+        return DataFrame(list(self.coll.find({'solbox_id': int(station_id)}).limit(limit)))
 
-    def load_all_stations_first_n(self, nrows=None):
-        data = list()
-        for station in self.station_ids:
-            data.append(self.load_single_station(self.coll, station, nrows))
-
+    def load_multiple_stations(self, station_ids, limit=0):
+        data = [self.load_single_station(station, limit) for station in station_ids] 
         return concat(data)
+
+    def load_all_stations(self, limit=0):
+        data = [self.load_single_station(station, limit) for station in self.station_ids] 
+        return concat(data)
+
+    def load_data_within(self, start, end):
+        return DataFrame(list(self.coll.find({'datetime': {'$gte': start, '$lt': end}}))).drop('_id', axis=1)
 
     def load_until(self, end=None, nrows=0):
         return DataFrame(list(self.coll.find({'datetime': {'$lt': end}}).limit(nrows))).drop('_id', axis=1)
@@ -26,27 +31,27 @@ class Measurements(MongoCollection):
     def load_from(self, start=None, nrows=0):
         return DataFrame(list(self.coll.find({'datetime': {'$gt': start}}).limit(nrows))).drop('_id', axis=1)
 
-    def get_first_measurement(self):
+    def load_first_measurement(self):
         return list(self.coll.find().sort('datetime', 1).limit(1))
 
     def get_last_measurement_single_station(self, station_id):
-        return list(self.coll.find({'solbox_id': station_id}).sort('datetime', 1).limit(1))
-    
-    def get_last_field_single_station(self, station_id, field): 
-        m = list(self.coll.find({'solbox_id': station_id}, {field}).sort('datetime', 1).limit(1))
+        return list(self.coll.find({'solbox_id': station_id}).sort('datetime', -1).limit(1))
+
+    def get_last_field_single_station(self, station_id, field):
+        m = list(self.coll.find({'solbox_id': station_id}, {field: 1}).sort('datetime', -1).limit(1))
         if not m:
             return None
 
         return m[0][field]
 
     def get_last_generator_measurement(self, station_id):
-        return self.get_last_field_single_station(station_id, 'fILoadDirect_avg')
+        return self.get_last_field_single_station(station_id, 'energy_production_i')
 
     def get_last_consumer_measurement(self, station_id):
-        return self.get_last_field_single_station(station_id, 'fIPV_avg')
+        return self.get_last_field_single_station(station_id, 'energy_consumption_i')
 
     def get_last_battery_measurement(self, station_id):
-        return self.get_last_field_single_station(station_id, 'u8StateOfBattery')
+        return self.get_last_field_single_station(station_id, 'battery_state_discrete')
 
     @property
     def station_ids(self):
