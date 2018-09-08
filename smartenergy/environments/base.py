04782e79793cacb9555160b565b95ceb39d0985a
javiermas <javier.mas@barcelonagse.eu>
2018-09-08 20:49:31 +0200

    :hankey: Add init_steps argument to environment

--- smartenergy/environments/base.py
+++ smartenergy/environments/base.py
@@ -5,11 +5,12 @@ from ..database import HourlyMeasurements, SimulatedMeasurements
 
 class Environment(ABC):
 
-    def __init__(self, ml_service, network, init_t, step_size,
+    def __init__(self, ml_service, network, init_t, init_steps, step_size,
                  source_repo=HourlyMeasurements(), mirror_repo=SimulatedMeasurements()):
         self.network = network
         self.ml_service = ml_service
         self.init_t = init_t
+        self.init_steps = init_steps
         self.t = self.init_t
         self.step_size = step_size
         self.source_repo = source_repo
