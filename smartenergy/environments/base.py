3edb8433e12c74866577a1dd4877a0c145acb9cb
javiermas <javier.mas@barcelonagse.eu>
2018-09-16 16:26:05 +0200

    :sparkles: :hammer: Refactor argument for initial steps and add MetricsManager

--- smartenergy/environments/base.py
+++ smartenergy/environments/base.py
@@ -1,20 +1,22 @@
 import logging
 from abc import abstractmethod, ABC
+
 from ..database import HourlyMeasurements, SimulatedMeasurements
+from .metrics_manager import MetricsManager
 
 
 class Environment(ABC):
 
-    def __init__(self, ml_service, network, init_t, init_steps, step_size,
+    def __init__(self, ml_service, network, burning_steps, init_steps, step_size,
                  source_repo=HourlyMeasurements(), mirror_repo=SimulatedMeasurements()):
         self.network = network
         self.ml_service = ml_service
-        self.init_t = init_t
+        self.burning_steps = burning_steps
         self.init_steps = init_steps
-        self.t = self.init_t
         self.step_size = step_size
         self.source_repo = source_repo
         self.mirror_repo = mirror_repo
+        self.metrics_manager = MetricsManager(self.source_repo)
     
     @abstractmethod
     def run(self, steps):
