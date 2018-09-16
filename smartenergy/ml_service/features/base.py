03f77925d882afe9b519f36c742ef8351200f3ed
javiermas <javier.mas@barcelonagse.eu>
2018-09-16 16:38:07 +0200

    :lipstick: :fire: Remove validation methods from base features and rename schema to schema_input

--- smartenergy/ml_service/features/base.py
+++ smartenergy/ml_service/features/base.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from ...exceptions import SchemaException
+
 
 class Transformer(ABC):
 
@@ -15,25 +15,8 @@ class Transformer(ABC):
 
     @property
     @abstractmethod
-    def schema(self):
+    def schema_input(self):
         pass
-
-    @staticmethod
-    def apply_schemata(func):
-        def new_func(self, data):
-            schema = self.schema
-            data_needed = data.copy()
-            if data.empty:
-                raise SchemaException(f'DataFrame passed to {self.__class__.__name__} was empty')
-            elif any(c not in data.columns for c in schema):
-                missing_column = next(c for c in schema if c not in data.columns)
-                raise SchemaException(f'DataFrame passed to {self.__class.__name__} missing the {missing_column} column')
-            else:
-                data_needed = data[[*schema.keys()]]
-
-            return func(self, data_needed)
-
-        return new_func
 
 
 class Feature(Transformer):
@@ -44,7 +27,7 @@ class Feature(Transformer):
 
     @property
     @abstractmethod
-    def schema(self):
+    def schema_input(self):
         pass
 
 
@@ -56,6 +39,5 @@ class Preprocessor(Transformer):
 
     @property
     @abstractmethod
-    def schema(self):
+    def schema_input(self):
         pass
-
