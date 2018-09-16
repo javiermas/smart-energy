0ad33b02d1672ae324a9531281ad1db97aea0119
javiermas <javier.mas@barcelonagse.eu>
2018-09-16 16:41:32 +0200

    :building_construction: Adapt features init to new files

--- smartenergy/ml_service/features/__init__.py
+++ smartenergy/ml_service/features/__init__.py
@@ -1,1 +1,6 @@
 from .lagged_readings import LaggedReadings
+from .basic_preprocessor import BasicPreprocessor
+from .battery_state_features import BatteryStateFeatures
+from .date_features import DateFeatures
+from .data_grouper import DataGrouper
+from .pipeline import Pipeline
