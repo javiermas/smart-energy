7894de764a78249ddcf1b9b10f3252542f5923c5
javiermas <javier.mas@barcelonagse.eu>
2018-09-16 16:39:37 +0200

    :sparkles: :bug: Adapt LaggedReadings to new naming of columns and fix small bug.

--- smartenergy/ml_service/features/lagged_readings.py
+++ smartenergy/ml_service/features/lagged_readings.py
@@ -1,4 +1,6 @@
+from numbers import Number
 from pandas import pivot_table
+
 from .base import Feature
 
 
@@ -7,13 +9,11 @@ class LaggedReadings(Feature):
     def __init__(self, lags):
         super().__init__()
         self.lags = lags
-        self.features_to_lag = ['fILoadDirect_avg', 'fILoad_avg', 'fIPV_avg',
-                                'fIExcess_avg', 'fIToGrid_avg', 'fIToBat_avg',
-                                'fTemperature_avg', 'u8StateOfBattery']
-        self.lagged_features = [f'{f}_lag_{lag}'.format(f, lag) for f in self.features_to_lag
+        self.features_to_lag = ['energy_generation_i', 'energy_consumption_i', 'energy_to_grid_i',
+                                'energy_to_battery_i', 'temperature', 'battery_state_discrete']
+        self.lagged_features = [f'{f}_lag_{lag}' for f in self.features_to_lag
                                 for lag in range(0, self.lags + 1)]
 
-    @Feature.apply_schemata
     def transform(self, data):
         features = data.copy()
         features = self.lag_features(features)
@@ -22,7 +22,7 @@ class LaggedReadings(Feature):
 
     def lag_features(self, data):
         for lag in range(self.lags + 1):
-            colnames = [f'{f}_lag_{lag}' for f in self.features_to_lag]
+            colnames = [f'{feature}_lag_{lag}' for feature in self.features_to_lag]
             data[colnames] = data.groupby('solbox_id')[self.features_to_lag].shift(lag).fillna(0)
 
         return data
@@ -34,17 +34,10 @@ class LaggedReadings(Feature):
         return data
     
     @property
-    def schema(self):
-        schema = {
+    def schema_input(self):
+        schema_core = {
             'solbox_id': int,
             'datetime': object,
-            'fILoadDirect_avg': float,
-            'fILoad_avg': float,
-            'fIPV_avg': float,
-            'fIExcess_avg': float,
-            'fIToGrid_avg': float,
-            'fIToBat_avg': float,
-            'fTemperature_avg': float,
-            'u8StateOfBattery': int,
         }
-        return schema
+        schema_features = {f: Number for f in self.features_to_lag}
+        return {**schema_core, **schema_features}
