e3fb55ae4ce6e6e8a5216a82d6253c445882e440
javiermas <javier.mas@barcelonagse.eu>
2018-09-18 16:44:23 +0200

    :fire: Remove InstallationGrid import from init file

--- smartenergy/network/installation/__init__.py
+++ smartenergy/network/installation/__init__.py
@@ -2,4 +2,4 @@ from .base import Installation
 from .battery import Battery
 from .generator import Generator
 from .consumer import Consumer
-from .installation_grid import InstallationGrid
+#from .installation_grid import InstallationGrid
