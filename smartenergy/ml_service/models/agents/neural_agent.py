9b9ed1d87bbbe6704bfe1426b65b2b2c96732437
javiermas <javier.mas@barcelonagse.eu>
2018-09-28 14:00:14 +0200

    :sparkles: Develop NeuralAgent

--- smartenergy/ml_service/models/agents/neural_agent.py
+++ smartenergy/ml_service/models/agents/neural_agent.py
@@ -1,20 +1,35 @@
+from random import choice
 import tensorflow as tf
+from numpy import argmax
 
 from .shallow_network import ShallowNetwork
 
 
 class NeuralAgent(object):
 
+    policies = {
+        'greedy': {
+            'action': argmax,
+            'reward': max,
+        }
+    }
+
     def __init__(self, epsilon, action_space, **network_kwargs):
         self.action_space = action_space
         self.shallow_network = ShallowNetwork(self.action_space, **network_kwargs)
         self.shallow_network.initialize()
+        self.policy = 'greedy'
+        self.action_func = self.policies[self.policy]['action']
+        self.reward_func = self.policies[self.policy]['reward']
 
     def train(self, memories):
         with tf.Session(graph=self.shallow_network.graph) as sess:
             action_nodes = self.dict_to_list_of_tuples(self.shallow_network.actions)
             reward_nodes = self.dict_to_list_of_tuples(self.shallow_network.reward)
-            nodes = [node[2] for node in action_nodes + reward_nodes]
+            #nodes = [node[2] for node in action_nodes + reward_nodes]
+            nodes = [
+                self.shallow_network.update
+            ]
             feed = {
                 self.shallow_network.state: state,
             }
@@ -25,20 +40,53 @@ class NeuralAgent(object):
             exp_rewards = {node[0]: {node[1]: reward} for node, reward in zip(reward_nodes, exp_rewards)}
             return actions
 
-    def get_action(self, state):
+    def get_action(self, state, random):
         with tf.Session(graph=self.shallow_network.graph) as sess:
-            action_nodes = self.dict_to_list_of_tuples(self.shallow_network.actions)
-            reward_nodes = self.dict_to_list_of_tuples(self.shallow_network.reward)
-            nodes = [node[2] for node in action_nodes + reward_nodes]
+            #action_nodes = self.dict_to_list_of_tuples(self.shallow_network.actions)
+            reward_nodes = self.dict_to_list_of_tuples(self.shallow_network.expected_reward)
+            nodes = [node[2] for node in reward_nodes]
             feed = {
                 self.shallow_network.state: state,
             }
             sess.run([tf.global_variables_initializer()], feed)
             output = sess.run(nodes, feed)
-            actions, exp_rewards = self.split_list(output, 2)
-            actions = {node[0]: {node[1]: float(action)} for node, action in zip(action_nodes, actions)}
-            exp_rewards = {node[0]: {node[1]: float(reward)} for node, reward in zip(reward_nodes, exp_rewards)}
+            #actions, exp_rewards = self.split_list(output, 2)
+            #actions = {node[0]: {node[1]: float(action)} for node, action in zip(action_nodes, actions)}
+            exp_rewards = {node[0]: {node[1]: reward.reshape(-1)} for node, reward in zip(reward_nodes, output)}
+            if random:
+                actions = self.get_random_action()
+            else:
+                actions = {node[0]: {node[1]: self.action_func(reward)}
+                           for node, reward in zip(reward_nodes, output)}
+
+            self.last_expected_reward = exp_rewards
+            self.last_state = state
+            self.last_actions = actions
             return actions
+
+    def get_state_value(self, state):
+        with tf.Session(graph=self.shallow_network.graph) as sess:
+            #action_nodes = self.dict_to_list_of_tuples(self.shallow_network.actions)
+            reward_nodes = self.dict_to_list_of_tuples(self.shallow_network.expected_reward)
+            nodes = [node[2] for node in reward_nodes]
+            feed = {
+                self.shallow_network.state: state,
+            }
+            sess.run([tf.global_variables_initializer()], feed)
+            output = sess.run(nodes, feed)
+            #actions, exp_rewards = self.split_list(output, 2)
+            #actions = {node[0]: {node[1]: float(action)} for node, action in zip(action_nodes, actions)}
+            exp_rewards = {node[0]: {node[1]: reward.reshape(-1)} for node, reward in zip(reward_nodes, output)}
+            return exp_rewards
+
+    def get_random_action(self):
+        actions = {}
+        for name, sub_space in self.action_space.items():
+            actions[name] = {}
+            for sub_space_name, sub_space_value in sub_space.items():
+                actions[name][sub_space_name] = choice(sub_space_value)
+
+        return actions
 
     @staticmethod
     def split_list(_list, n_splits=1):
