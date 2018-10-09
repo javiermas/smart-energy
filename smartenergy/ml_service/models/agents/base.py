6192d87d2d58a259be9e1d8772b2f7574b704d90
javiermas <javier.mas@barcelonagse.eu>
2018-10-09 14:17:53 +0200

    :sparkles: Develop base agents

--- smartenergy/ml_service/models/agents/base.py
+++ smartenergy/ml_service/models/agents/base.py
@@ -1,6 +1,21 @@
-class Agent:
-    pass
+from abc import abstractmethod
+
+from ..base import Model
 
 
-class Network:
+class Agent(Model):
+
+    def __call__(self):
+        self.train()
+
+    @abstractmethod
+    def train(self):
+        pass
+    
+    @abstractmethod
+    def get_action(self):
+        pass
+    
+
+class Network(object):
     pass
