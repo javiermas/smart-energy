27ba134baba294086ea98f9a633177a2dede8334
javiermas <javier.mas@barcelonagse.eu>
2018-10-10 14:10:27 +0200

    :sparkles: Improve logging with class name

--- smartenergy/ml_service/models/base.py
+++ smartenergy/ml_service/models/base.py
@@ -6,7 +6,7 @@ from ...logger import Logger
 class Model(ABC):
 
     def __init__(self):
-        self.log = Logger()
+        self.log = Logger(self.__class__.__name__)
 
     @abstractmethod
     def train(self):
