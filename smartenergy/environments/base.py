cac6d5775b248f7712f40c0d0cc207798a521715
javiermas <javier.mas@barcelonagse.eu>
2018-10-10 14:04:44 +0200

    :hammer: Replace SimulatedMeasurements by DataStream in base environment

--- smartenergy/environments/base.py
+++ smartenergy/environments/base.py
@@ -2,23 +2,20 @@ from abc import abstractmethod, ABC
 
 from .metrics_manager import MetricsManager
 from ..logger import Logger
-from ..database import HourlyMeasurements, SimulatedMeasurements
 
 
 
 class Environment(ABC):
 
-    def __init__(self, ml_service, network, burning_steps, init_steps, step_size,
-                 source_repo=HourlyMeasurements(), mirror_repo=SimulatedMeasurements()):
-        self.log = Logger()
+    def __init__(self, ml_service, network, data_stream, burning_steps, init_steps, step_size):
+        self.log = Logger(self.__class__.__name__)
         self.network = network
         self.ml_service = ml_service
+        self.data_stream = data_stream
         self.burning_steps = burning_steps
         self.init_steps = init_steps
         self.step_size = step_size
-        self.source_repo = source_repo
-        self.mirror_repo = mirror_repo
-        self.metrics_manager = MetricsManager(self.source_repo)
+        self.metrics_manager = MetricsManager(self.data_stream)
     
     @abstractmethod
     def run(self, steps):
